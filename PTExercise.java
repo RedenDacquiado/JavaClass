6679dd252e06446571acf56cc0783bd3c0db6c42
RedenDacquiado <Rijdacquiado@mcm.edu.ph>
2025-04-23 13:57:59 +0800

    commit

---
+++ PTExercise.java
@@ -0,0 +1,10 @@
+public class PTExercise {
+    public static void main(String []args) {
+        for (int i = 1; i <= 10; i++) {
+            if (i == 8) {
+                break; // Stop the loop when i is 7
+            }
+            System.out.println("Value: " + i);
+        }
+    }
+}
