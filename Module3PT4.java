d48d042dd615589eeb525a86b8e794b90750a8e0
RedenDacquiado <Rijdacquiado@mcm.edu.ph>
2025-04-23 21:50:37 +0800

    commit

--- Module3PT4.java
+++ Module3PT4.java
@@ -7,11 +7,23 @@ public class Module3PT4 {
         System.out.println("Second Element: " + names[1]);
         System.out.println("Third Element: " + names[2]);
 
+        System.out.println("Max Value Array:");
+
+        for (int i = 0; i < names.length; i++) {
+            System.out.println("Element at index " + i + ": " + names[i]);
+        }
+
         System.out.println("New Array:");
 
         names[1] = "James";
         System.out.println("First Element: " + names[0]);
         System.out.println("Updated Second Element: " + names[1]);
         System.out.println("Third Element: " + names[2]);
+
+        System.out.println("Max Value Array:");
+
+        for (int i = 0; i < names.length; i++) {
+            System.out.println("Element at index " + i + ": " + names[i]);
+        }
     }
 }
