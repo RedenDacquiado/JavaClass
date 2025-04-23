815fc4d8cf12598c274fbce0f598249977ae1e7f
RedenDacquiado <Rijdacquiado@mcm.edu.ph>
2025-04-23 21:44:51 +0800

    commit

--- Module3PT4.java
+++ Module3PT4.java
@@ -1,12 +1,17 @@
 public class Module3PT4 {
     public static void main(String[] args) {
         String [] names = {"Joshua", "Julian", "Ester", "John", "Mary"};
+        System.out.println("Old Array:");
 
-        System.out.println("First element: " + names[0]);
+        System.out.println("First Element: " + names[0]);
+        System.out.println("Second Element: " + names[1]);
+        System.out.println("Third Element: " + names[2]);
+
+        System.out.println("New Array:");
 
         names[1] = "James";
-        System.out.println("Updated Second element: " + names[1]);
-
-        System.out.println("Third element: " + names[2]);
+        System.out.println("First Element: " + names[0]);
+        System.out.println("Updated Second Element: " + names[1]);
+        System.out.println("Third Element: " + names[2]);
     }
 }
